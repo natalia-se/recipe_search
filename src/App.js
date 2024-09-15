f9e81b48f430bb36d62b2eb6db43e48db31d6168
Nataliia Ishchenko <nataliia.ishchenko@desifer.se>
2024-09-15 15:24:39 +0200

    add app id and key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Card from './Components/Card';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./Components/Card";
 
 function App() {
   /* updates with your Edamam api-id and key from .env file */
@@ -9,21 +9,23 @@ function App() {
 
   /* useState to keep the state of the recipes and search in the app */
   const [foodRecipes, setFoodRecipes] = useState([]);
-  const [recipeSearch, setRecipeSearch] = useState('sprinkled donut');
-  const [searchQuery, setSearchQuery] = useState('sprinkled donut');
+  const [recipeSearch, setRecipeSearch] = useState("sprinkled donut");
+  const [searchQuery, setSearchQuery] = useState("sprinkled donut");
 
   /* every time the searchQuery is changed the useEffect is called */
   useEffect(() => {
     /* fetch used to get recipes from API. */
     const getRecipesFunction = async () => {
       // Search call built with search query and your unique app-id and key.
-      const response = await fetch(`https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_KEY}`  );
+      const response = await fetch(
+        `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_KEY}`
+      );
 
-      // waitxfor the respose from the api and changes it to json-format 
-      const data = await response.json(); 
+      // waitxfor the respose from the api and changes it to json-format
+      const data = await response.json();
 
       // update the state of the recipes shown in window
-      setFoodRecipes(data.hits); 
+      setFoodRecipes(data.hits);
     };
 
     getRecipesFunction();
@@ -37,31 +39,30 @@ function App() {
   const searchOnSubmit = (e) => {
     e.preventDefault();
     setSearchQuery(recipeSearch);
-    setRecipeSearch('');
+    setRecipeSearch("");
   };
 
   return (
     <div>
-      <div className='search-bar'>
+      <div className="search-bar">
         <form onSubmit={searchOnSubmit}>
           <input
-              type="text"
-              name="search"
-              value={recipeSearch}
-              onChange={updateSearchOnChange}
-              placeholder="Search"
+            type="text"
+            name="search"
+            value={recipeSearch}
+            onChange={updateSearchOnChange}
+            placeholder="Search"
           />
-          <button type="submit">
-              Search
-          </button>
+          <button type="submit">Search</button>
         </form>
       </div>
-      <div className='recipe-container'>
+      <div className="recipe-container">
         {/* the map function allows you to map out each of recipe in the array returned from the api */}
-        {foodRecipes && foodRecipes?.map(item => {
-          // each recipe is sent to the Card component which recives it as a prop
-          return <Card recipe={item.recipe} />
-        })}
+        {foodRecipes &&
+          foodRecipes?.map((item) => {
+            // each recipe is sent to the Card component which recives it as a prop
+            return <Card recipe={item.recipe} />;
+          })}
       </div>
     </div>
   );
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
+import React from "react";
 
-export default function Card({recipe}) {
+export default function Card({ recipe }) {
   // the repice prop sent from App.js is recived and deconstructed for easy use in component
 
   // hit F12, or right click page and select "inspect" to open developer tools in the broswer. Select "console" and look at the recipe object
-  console.log(recipe) // prints to the console
+  console.log(recipe); // prints to the console
 
   // TODO
   // * complete the src-atribute on the img-tag to show the recipe picture
   // * complete the href-attribute #top on the link a-tag to the proper recipe url
   // * update the card with information you'd find interesting about the recipe
   // * update the card styling
-  
+
   return (
-    <div className='recipe-card'>
-      <img src="" alt={recipe.label} />
+    <div className="recipe-card">
+      <img className="recipe-img" src={recipe.image} alt={recipe.label} />
       <h2>{recipe.label}</h2>
-      <a href="#top" className='button'>To recipe</a>
+      <p>{recipe.source}</p>
+      <a href={recipe.url} className="button">
+        To recipe
+      </a>
     </div>
-  )
+  );
 }
